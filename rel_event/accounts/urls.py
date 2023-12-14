6288a18af3ab49d87d0de71a7e11f4c70ce08800
ROHITH <rohithburle16@gmail.com>
2023-12-14 14:29:33 +0530

    deleted signals.py in broadcasts

--- rel_event/accounts/urls.py
+++ rel_event/accounts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('password-reset/', PasswordResetAPIView.as_view(), name='password_reset_api'),
-    path('password-update/<int:pk>/<token>/', PasswordUpdateAPIView.as_view(), name='password_update_api'),
+    path('password-update/<int:pk>/<token>/', PasswordUpdateAPIView.as_view(), name='password_update_api'),  #add frontend url later
 ]
