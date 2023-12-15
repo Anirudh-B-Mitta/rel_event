7a19d6e4d7df6d2f2c3d164ef37dd46b0f0668f6
ROHITH <rohithburle16@gmail.com>
2023-12-15 13:54:54 +0530

    updated event models.py

--- rel_event/events/models.py
+++ rel_event/events/models.py
@@ -11,6 +11,20 @@ class Event(models.Model):
     require_volunteers = models.BooleanField(default=False)
     poster = models.ImageField(upload_to='event_posters/', null=True, blank=True)
     ticket_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=510, null =True) 
+    medium_choices=(("offline","Offline"),("online","Online"))
+    medium = models.CharField(max_length=10, choices=medium_choices,default="offline")
+    category_choices=(("music","Music"),
+        ("games","Games"),
+        ("sports","Sports"),
+        ("arts","Arts"),
+        ("film","Film"),
+        ("literature","Literature"),
+        ("technology","Technology"),
+        ("other","Other")
+    )
+    category = models.CharField(max_length=10, choices=category_choices,default="other")
+    duration = models.DecimalField(max_digits=2,decimal_places=2,null =True)
 
     def __str__(self):
         return self.event_name
