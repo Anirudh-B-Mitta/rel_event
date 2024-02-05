e7154ce841895b9f06c6338a1adbe77f587b7635
Siddartha <siddartharedddyl@gmail.com>
2024-02-05 14:46:24 +0530

    added database

--- rel_event/rel_event/settings.py
+++ rel_event/rel_event/settings.py
@@ -91,24 +91,24 @@ WSGI_APPLICATION = "rel_event.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-# import os
 # DATABASES = {
 #     "default": {
-#         "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.mysql"),
-#         "NAME": os.environ.get("DB_NAME", "django_testing"),
-#         "USER": os.environ.get("DB_USER", "admin"),
-#         "PASSWORD": os.environ.get("DB_PASSWORD", "admin"),
-#         "HOST": os.environ.get("DB_HOST", "mysql"),
-#         "PORT": os.environ.get("DB_PORT", "3306"),
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+import os
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.mysql"),
+        "NAME": os.environ.get("DB_NAME", "django_testing"),
+        "USER": os.environ.get("DB_USER", "admin"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "admin"),
+        "HOST": os.environ.get("DB_HOST", "mysql"),
+        "PORT": os.environ.get("DB_PORT", "3306"),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
