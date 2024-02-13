580c4342c23d564b687710088eaf535ee8940aac
Anirudh <jcuasghduhy@gcu.com>
2024-02-13 14:48:11 +0530

    Changed self channels

--- rel_event/volunteers/serializers.py
+++ rel_event/volunteers/serializers.py
@@ -6,3 +6,24 @@ class VolunteerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Volunteer
         fields = '__all__'
+
+# volunteers/serializers.py
+from rest_framework import serializers
+from events.models import Event
+from accounts.serializers import UserSerializer
+from broadcast.models import Broadcast
+# from .models import Volunteers
+
+class EventUserSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+class CombinedDataSerializer(serializers.ModelSerializer):
+    event = EventUserSerializer()
+
+    class Meta:
+        model = Broadcast
+        fields = '__all__'
--- rel_event/volunteers/views.py
+++ rel_event/volunteers/views.py
@@ -1,7 +1,7 @@
 # volunteers/views.py
 from rest_framework import generics
 from .models import Volunteer
-from .serializers import VolunteerSerializer
+from .serializers import VolunteerSerializer, CombinedDataSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -22,14 +22,15 @@ class YourVolunteerDetailView(generics.RetrieveDestroyAPIView):
     serializer_class = VolunteerSerializer
 
 class SubscribedChannels(APIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     def get(self, request, user_id):
         # Retrieve all broadcast channels for the given user_id
         channels = Volunteer.objects.filter(user_id=user_id).values_list('event_id', flat=True)
+        # volunteers = Volunteer.objects.select_related('user', 'event', 'broadcast')
         print(channels)
 
         # Query the Broadcast model to get details of the subscribed channels
-        subscribed_channels = Broadcast.objects.filter(event_id__in=channels)
-        serializer = BroadcastSerializer(subscribed_channels, many=True)
+        subscribed_channels = Broadcast.objects.filter(event_id__in=channels).select_related('event', 'event__user')
+        serializer = CombinedDataSerializer(subscribed_channels, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
